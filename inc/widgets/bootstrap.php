9c164ac9a02666c87a396d088b3135972ec94629
Ramya krishna <ramyakrishnaimminni@gmail.com>
2026-02-16 14:20:40 +0530

    fixes

--- inc/widgets/bootstrap.php
+++ inc/widgets/bootstrap.php
@@ -9,7 +9,7 @@ namespace Bigrock\Widgets;
 
 use Bigrock\Widgets\SitePreview;
 
-require_once BIGROCK_PLUGIN_DIR . '/inc/Widgets/SitePreview.php';
+require_once BIGROCK_PLUGIN_DIR . '/inc/widgets/SitePreview.php';
 
 /* Start up the Dashboards */
 if ( is_admin() ) {
