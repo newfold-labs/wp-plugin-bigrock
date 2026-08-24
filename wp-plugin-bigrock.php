aa5357fac522b8d981016d5e2fe0f9b4d4842858
Ajay Yadav <ajayadav09@gmail.com>
2026-08-25 02:40:20 +0530

    updated wordpress version to 7.1

--- wp-plugin-bigrock.php
+++ wp-plugin-bigrock.php
@@ -15,7 +15,7 @@
  * Version:           1.0.2
  * Requires at least: 6.6
  * Requires PHP:      7.4
- * Tested up to:      7.0
+ * Tested up to:      7.1
  * Author:            BigRock
  * Author URI:        https://www.bigrock.in
  * Text Domain:       wp-plugin-bigrock
